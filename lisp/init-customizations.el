e7e1864cb900e3366483d36e79e8270fd6f2ac12
Ben Bass <benbass@Ben-Bass-MacBook-Air.local>
2017-03-28 15:44:58 -0700

    Add diary file and journal dir to stop prompts
    When typing 'M-x calendar', emacs could complian about a diary file or
    a journal directory. Create these in machine-specific/ until I know
    hwat to do with them.

--- lisp/init-customizations.el
+++ lisp/init-customizations.el
@@ -3,7 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(diary-file "~/.emacs.d/machine-specific/")
+ '(org-journal-dir "~/.emacs.d/machine-specific/")
+ '(smex-save-file "~/.emacs.d/machine-specific/smex-items"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
