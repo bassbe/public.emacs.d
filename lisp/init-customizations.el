a41d681ed5add1343d63a59588151f0a5f918622
Benjamin Bass <wesleybass42@gmail.com>
2017-03-28 22:11:14 -0700

    Enable Visible Bell

--- lisp/init-customizations.el
+++ lisp/init-customizations.el
@@ -2,10 +2,7 @@
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(diary-file "~/.emacs.d/machine-specific/")
- '(org-journal-dir "~/.emacs.d/machine-specific/")
- '(smex-save-file "~/.emacs.d/machine-specific/smex-items"))
+ ;; If there is more than one, they won't work right.)
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
