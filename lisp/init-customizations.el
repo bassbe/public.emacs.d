54a939fd927a258f0c380543dbd9677e81ddb2b5
Ben Bass <benbass@Ben-Bass-MacBook-Air.local>
2017-04-04 13:27:43 -0700

    Update Smex, init-custom, and readme

--- lisp/init-customizations.el
+++ lisp/init-customizations.el
@@ -1,12 +1,11 @@
+;; (provide 'init-customizations.el)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (znc web-mode use-package tramp-theme tomatinho tea-time smex smartparens smart-mode-line-powerline-theme slime reveal-in-osx-finder redtick rainbow-delimiters rainbow-blocks prodigy popwin pomodoro pomidor plantuml-mode pallet org-pomodoro org-journal org-bullets nyan-mode neotree multiple-cursors minimap magit jade jabber idle-highlight-mode htmlize highlight-parentheses highlight helm-projectile gnuplot-mode gnuplot flycheck-elm flycheck-cask flx-ido fill-column-indicator expand-region exec-path-from-shell emms elm-mode eclim drag-stuff dired-sort dired-rainbow dired+ dionysos darkroom csv-mode crux column-marker color-theme-sanityinc-tomorrow color-theme circe calfw btc-ticker bbdb-csv-import auto-complete auctex ahungry-theme ace-window ace-jump-mode))))
->>>>>>> Stashed changes
+ '(dired-use-ls-dired (quote unspecified)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
