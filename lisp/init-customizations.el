0c21471dfa8aba569721242d9054ed215c0c648a
Benjamin Bass <wesleybass42@gmail.com>
2017-06-15 11:51:58 -0700

    Some customizations change

--- lisp/init-customizations.el
+++ lisp/init-customizations.el
@@ -5,7 +5,32 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(LaTeX-amsmath-label "eq:"))
+ '(LaTeX-amsmath-label "eq:")
+ '(erc-modules
+   (quote
+    (autojoin button completion fill irccontrols keep-place list log match menu move-to-prompt netsplit networks noncommands readonly ring services stamp track)))
+ '(org-latex-default-packages-alist
+   (quote
+    (("spanish, russian, english" "babel" nil)
+     ("utf8" "inputenc" t)
+     ("T1" "fontenc" t)
+     ("" "fixltx2e" nil)
+     ("" "graphicx" t)
+     ("" "longtable" nil)
+     ("" "float" nil)
+     ("" "wrapfig" nil)
+     ("" "rotating" nil)
+     ("normalem" "ulem" t)
+     ("" "amsmath" t)
+     ("" "textcomp" t)
+     ("" "marvosym" t)
+     ("" "wasysym" t)
+     ("" "amssymb" t)
+     ("" "hyperref" nil)
+     "\\tolerance=1000")))
+ '(package-selected-packages
+   (quote
+    (znc web-mode use-package tramp-theme tomatinho tea-time smex smartparens smart-mode-line-powerline-theme slime reveal-in-osx-finder redtick rainbow-delimiters rainbow-blocks prodigy popwin pomodoro pomidor plantuml-mode pallet org-pomodoro org-journal org-bullets nyan-mode neotree multiple-cursors minimap magit jade jabber idle-highlight-mode htmlize highlight-parentheses highlight helm-projectile gnuplot-mode gnuplot flycheck-elm flycheck-cask flx-ido fill-column-indicator expand-region exec-path-from-shell emms elm-mode eclim drag-stuff dired-sort dired-rainbow dired+ dionysos darkroom csv-mode crux column-marker color-theme-sanityinc-tomorrow color-theme circe calfw btc-ticker browse-kill-ring bbdb-csv-import auto-complete auctex ahungry-theme ace-window ace-jump-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
