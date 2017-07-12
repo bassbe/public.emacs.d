1f6b1b4882f340a33e683bc9bd21b36cec3df736
Benjamin Bass <wesleybass42@gmail.com>
2017-07-11 18:08:03 -0700

    Auto Generated file update

--- lisp/init-customizations.el
+++ lisp/init-customizations.el
@@ -5,9 +5,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
+ '(org-modules
    (quote
-    (znc web-mode use-package tramp-theme tomatinho tea-time smex smartparens smart-mode-line-powerline-theme slime reveal-in-osx-finder redtick rainbow-delimiters rainbow-blocks prodigy popwin pomodoro pomidor plantuml-mode peep-dired pallet org-pomodoro org-journal org-bullets nyan-mode neotree multiple-cursors minimap magit js2-mode jabber idle-highlight-mode htmlize highlight-parentheses highlight helm-projectile gnuplot-mode gnuplot flycheck-elm flycheck-cask flx-ido fill-column-indicator expand-region exec-path-from-shell emms elm-mode eclim drag-stuff doom-themes dired-sort dired-rainbow dired+ dionysos darkroom csv-mode crux company column-marker color-theme-sanityinc-tomorrow color-theme circe calfw btc-ticker browse-kill-ring bbdb-csv-import auto-complete auctex ahungry-theme ace-window ace-jump-mode))))
+    (org-bbdb org-bibtex org-docview org-gnus org-info org-irc org-mhe org-rmail org-w3m org-mac-link))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
